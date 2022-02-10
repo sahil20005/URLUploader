32c47b44279b46ad4e63a97f44d50e1c2ff3c66a
sahil20005 <90039627+sahil20005@users.noreply.github.com>
2022-02-10 14:17:11 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,16 +46,5 @@ async def start(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.START_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Source", url="https://github.com/X-Gorn/X-URL-Uploader"
-                        ),
-                        InlineKeyboardButton("Project Channel", url="https://t.me/xTeamBots"),
-                    ],
-                    [InlineKeyboardButton("Author", url="https://t.me/xgorn")],
-                ]
-            ),
             reply_to_message_id=update.message_id
         )
